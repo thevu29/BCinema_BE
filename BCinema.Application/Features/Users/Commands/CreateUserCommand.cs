f64fe0f55fa93689c983752a786f8837654ef4cf
thevu29 <vnguyen132az@gmail.com>
2024-11-09 10:02:39 +0700

    fix upload/update image

--- BCinema.Application/Features/Users/Commands/CreateUserCommand.cs
+++ BCinema.Application/Features/Users/Commands/CreateUserCommand.cs
@@ -18,49 +18,34 @@ namespace BCinema.Application.Features.Users.Commands
         public IFormFile? Avatar { get; set; }
         public Guid RoleId { get; set; }
 
-        public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserDto>
+        public class CreateUserCommandHandler(
+            IUserRepository userRepository,
+            IRoleRepository roleRepository,
+            IPasswordHasher<User> passwordHasher,
+            IMapper mapper,
+            IFileStorageService fileStorageService) : IRequestHandler<CreateUserCommand, UserDto>
         {
-            private readonly IUserRepository _userRepository;
-            private readonly IRoleRepository _roleRepository;
-            private readonly IPasswordHasher<User> _passwordHasher;
-            private readonly IMapper _mapper;
-            private readonly IFileStorageService _fileStorageService;
-
-            public CreateUserCommandHandler(
-                IUserRepository userRepository,
-                IRoleRepository roleRepository,
-                IPasswordHasher<User> passwordHasher,
-                IMapper mapper,
-                IFileStorageService fileStorageService)
-            {
-                _userRepository = userRepository;
-                _roleRepository = roleRepository;
-                _passwordHasher = passwordHasher;
-                _mapper = mapper;
-                _fileStorageService = fileStorageService;
-            }
-
             public async Task<UserDto> Handle(CreateUserCommand request, CancellationToken cancellationToken)
             {
-                var role = await _roleRepository
+                var role = await roleRepository
                     .GetByIdAsync(request.RoleId, cancellationToken)
                     ?? throw new NotFoundException(nameof(Role));
 
-                var user = _mapper.Map<User>(request);
+                var user = mapper.Map<User>(request);
 
-                user.Password = _passwordHasher.HashPassword(user, request.Password);
+                user.Password = passwordHasher.HashPassword(user, request.Password);
 
                 if (request.Avatar != null)
                 {
-                    using var imageStream = request.Avatar.OpenReadStream();
-                    user.Avatar = await _fileStorageService.UploadImageAsync(
-                        imageStream, "avatars/" + Guid.NewGuid() + ".jpg");
+                    await using var imageStream = request.Avatar.OpenReadStream();
+                    user.Avatar = await fileStorageService.UploadImageAsync(
+                        imageStream, Guid.NewGuid() + ".jpg");
                 }
 
-                await _userRepository.AddAsync(user, cancellationToken);
-                await _userRepository.SaveChangesAsync(cancellationToken);
+                await userRepository.AddAsync(user, cancellationToken);
+                await userRepository.SaveChangesAsync(cancellationToken);
 
-                return _mapper.Map<UserDto>(user);
+                return mapper.Map<UserDto>(user);
             }
         }
     }
--- BCinema.Application/Features/Users/Commands/UpdateUserCommand.cs
+++ BCinema.Application/Features/Users/Commands/UpdateUserCommand.cs
@@ -16,49 +16,38 @@ namespace BCinema.Application.Features.Users.Commands
         public IFormFile? Avatar { get; set; }
         public Guid? RoleId { get; set; }
         
-        public class UpdateUserCommandHanlder : IRequestHandler<UpdateUserCommand, UserDto>
+        public class UpdateUserCommandHandler(
+            IUserRepository userRepository,
+            IRoleRepository roleRepository,
+            IFileStorageService fileStorageService,
+            IMapper mapper) : IRequestHandler<UpdateUserCommand, UserDto>
         {
-            private readonly IUserRepository _userRepository;
-            private readonly IRoleRepository _roleRepository;
-            private readonly IFileStorageService _fileStorageService;
-            private readonly IMapper _mapper;
-
-            public UpdateUserCommandHanlder(
-                IUserRepository userRepository,
-                IRoleRepository roleRepository,
-                IFileStorageService fileStorageService,
-                IMapper mapper)
-            {
-                _userRepository = userRepository;
-                _roleRepository = roleRepository;
-                _fileStorageService = fileStorageService;
-                _mapper = mapper;
-            }
-
             public async Task<UserDto> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
             {
-                var user = await _userRepository
+                var user = await userRepository
                     .GetByIdAsync(request.Id, cancellationToken)
                     ?? throw new NotFoundException(nameof(User));
 
                 if (request.Avatar != null)
                 {
-                    using var imageStream = request.Avatar.OpenReadStream();
-                    user.Avatar = await _fileStorageService.UploadImageAsync(
-                        imageStream, "avatars/" + Guid.NewGuid() + ".jpg");
+                    await using var imageStream = request.Avatar.OpenReadStream();
+                    
+                    user.Avatar = !string.IsNullOrEmpty(user.Avatar) 
+                        ? await fileStorageService.UpdateImageAsync(imageStream, Guid.NewGuid() + ".jpg", user.Avatar)
+                        : await fileStorageService.UploadImageAsync(imageStream, Guid.NewGuid() + ".jpg");
                 }
 
                 if (request.RoleId != null)
                 {
-                    var role = await _roleRepository.GetByIdAsync(request.RoleId.Value, cancellationToken)
+                    var role = await roleRepository.GetByIdAsync(request.RoleId.Value, cancellationToken)
                         ?? throw new NotFoundException(nameof(Role));
                 }
 
-                _mapper.Map<User>(request);
+                mapper.Map<User>(request);
 
-                await _userRepository.SaveChangesAsync(cancellationToken);
+                await userRepository.SaveChangesAsync(cancellationToken);
 
-                return _mapper.Map<UserDto>(user);
+                return mapper.Map<UserDto>(user);
             }
         }
     }   
