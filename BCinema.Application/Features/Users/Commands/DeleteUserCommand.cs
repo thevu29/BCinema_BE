aede30ccf09f4d040af0ca389bbe12dde721baa7
thevu29 <vnguyen132az@gmail.com>
2024-11-09 21:30:26 +0700

    fix cannot delete admin role

--- BCinema.Application/Features/Users/Commands/DeleteUserCommand.cs
+++ BCinema.Application/Features/Users/Commands/DeleteUserCommand.cs
@@ -9,23 +9,21 @@ public class DeleteUserCommand : IRequest
 {
     public Guid Id { get; set; }
 
-    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+    public class DeleteUserCommandHandler(IUserRepository userRepository) : IRequestHandler<DeleteUserCommand>
     {
-        private readonly IUserRepository _userRepository;
-        
-        public DeleteUserCommandHandler(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
-
         public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userRepository.GetByIdAsync(request.Id, cancellationToken)
+            var user = await userRepository.GetByIdAsync(request.Id, cancellationToken)
                 ?? throw new NotFoundException(nameof(User));
+            
+            if (user.Role.Name.ToLower().Equals("admin"))
+            {
+                throw new BadRequestException("Admin cannot be deleted");
+            }
 
             user.DeleteAt = DateTime.UtcNow;
 
-            await _userRepository.SaveChangesAsync(cancellationToken);
+            await userRepository.SaveChangesAsync(cancellationToken);
         }
     }
 }
