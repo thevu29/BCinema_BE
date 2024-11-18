f83ba445eaa16858f29ed835e6781f96adb35421
thevu29 <vnguyen132az@gmail.com>
2024-11-18 16:11:15 +0700

    add check user use voucher by code

---
+++ BCinema.Application/Features/UserVouchers/Queries/GetUserVoucherByUIdAndVCodeQuery.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BCinema.Application.DTOs;
+using BCinema.Application.Exceptions;
+using BCinema.Domain.Entities;
+using BCinema.Domain.Interfaces.IRepositories;
+using MediatR;
+
+namespace BCinema.Application.Features.UserVouchers.Queries;
+
+public class GetUserVoucherByUIdAndVCodeQuery : IRequest<UserVoucherDto?>
+{
+    public Guid UserId { get; set; }
+    public string Code { get; set; } = default!;
+
+    public class GetUserVoucherByUIdAndVCodeQueryHandler(
+        IUserVoucherRepository userVoucherRepository,
+        IUserRepository userRepository,
+        IVoucherRepository voucherRepository,
+        IMapper mapper) : IRequestHandler<GetUserVoucherByUIdAndVCodeQuery, UserVoucherDto?>
+    {
+        public async Task<UserVoucherDto?> Handle(GetUserVoucherByUIdAndVCodeQuery request, CancellationToken cancellationToken)
+        {
+            var user = await userRepository.GetByIdAsync(request.UserId, cancellationToken) 
+                       ?? throw new NotFoundException(nameof(User));
+
+            var voucher = await voucherRepository.GetByCoedAsync(request.Code, cancellationToken)
+                          ?? throw new NotFoundException(nameof(Voucher));
+
+            var userVoucher = await userVoucherRepository
+                .GetUserVoucherByUIdAndVIdAsync(user.Id, voucher.Id, cancellationToken);
+
+            return userVoucher != null ? mapper.Map<UserVoucherDto>(userVoucher) : null; 
+        }
+    }
+}
--- BCinema.Infrastructure/Repositories/UserVoucherRepository.cs
+++ BCinema.Infrastructure/Repositories/UserVoucherRepository.cs
@@ -5,23 +5,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BCinema.Infrastructure.Repositories
 {
-    public class UserVoucherRepository : IUserVoucherRepository
+    public class UserVoucherRepository(ApplicationDbContext context) : IUserVoucherRepository
     {
-        private readonly ApplicationDbContext _context;
-
-        public UserVoucherRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
         public IQueryable<UserVoucher> GetUserVouchers()
         {
-            return _context.UserVouchers;
+            return context.UserVouchers;
         }
 
         public async Task<IEnumerable<UserVoucher>> GetUserVouchersAsync(CancellationToken cancellationToken)
         {
-            return await _context.UserVouchers
+            return await context.UserVouchers
                 .Include(x => x.User)
                 .Include(x => x.Voucher)
                 .ToListAsync(cancellationToken);
@@ -30,17 +23,28 @@ namespace BCinema.Infrastructure.Repositories
         public async Task<UserVoucher?> GetUserVoucherByUIdAndVIdAsync(
            Guid userId, Guid voucherId, CancellationToken cancellationToken)
         {
-            return await _context.UserVouchers
+            return await context.UserVouchers
                 .Include(x => x.User)
                 .Include(x => x.Voucher)
                 .FirstOrDefaultAsync(x => x.UserId == userId && x.VoucherId == voucherId, cancellationToken);
+        }
+        
+        public async Task<UserVoucher?> GetUserVoucherByUIdAndVCodeAsync(
+            Guid userId,
+            string code,
+            CancellationToken cancellationToken)
+        {
+            return await context.UserVouchers
+                .Include(x => x.User)
+                .Include(x => x.Voucher)
+                .FirstOrDefaultAsync(x => x.UserId == userId && x.Voucher.Code == code, cancellationToken);
         }
 
         public async Task<IEnumerable<UserVoucher>> GetUserVouchersByUIdAsync(
             Guid userId,
             CancellationToken cancellationToken)
         {
-            return await _context.UserVouchers
+            return await context.UserVouchers
                 .Include(x => x.User)
                 .Include(x => x.Voucher)
                 .Where(x => x.UserId == userId)
@@ -51,18 +55,18 @@ namespace BCinema.Infrastructure.Repositories
             UserVoucher userVoucher,
             CancellationToken cancellationToken)
         {
-            await _context.UserVouchers.AddAsync(userVoucher, cancellationToken);
+            await context.UserVouchers.AddAsync(userVoucher, cancellationToken);
             return userVoucher;
         }
 
         public async Task SaveChangesAsync()
         {
-            await _context.SaveChangesAsync();
+            await context.SaveChangesAsync();
         }
 
         public async Task SaveChangesAsync(CancellationToken cancellationToken)
         {
-            await _context.SaveChangesAsync(cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
         }
     }
 }
