919e3eef716242c20213af453bc1bcb059e179a5
thevu29 <vnguyen132az@gmail.com>
2024-12-09 10:10:54 +0700

    add food image field

--- BCinema.Application/Features/Foods/Commands/UpdateFoodCommand.cs
+++ BCinema.Application/Features/Foods/Commands/UpdateFoodCommand.cs
@@ -3,7 +3,9 @@ using BCinema.Application.DTOs;
 using BCinema.Application.Exceptions;
 using BCinema.Domain.Entities;
 using BCinema.Domain.Interfaces.IRepositories;
+using BCinema.Domain.Interfaces.IServices;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace BCinema.Application.Features.Foods.Commands;
 
@@ -13,17 +15,30 @@ public class UpdateFoodCommand : IRequest<FoodDto>
     public string? Name { get; set; }
     public double? Price { get; set; }
     public int? Quantity { get; set; }
+    public IFormFile? Image { get; set; }
 
-    public class UpdateFoodCommandHandler(IFoodRepository foodRepository, IMapper mapper)
-        : IRequestHandler<UpdateFoodCommand, FoodDto>
+    public class UpdateFoodCommandHandler(
+        IFoodRepository foodRepository,
+        IFileStorageService fileStorageService,
+        IMapper mapper) : IRequestHandler<UpdateFoodCommand, FoodDto>
     {
         public async Task<FoodDto> Handle(UpdateFoodCommand request, CancellationToken cancellationToken)
         {
             var food = await foodRepository.GetFoodByIdAsync(request.Id, cancellationToken)
                 ?? throw new NotFoundException(nameof(Food));
 
-            mapper.Map(request, food);
+            
+            if (request.Image != null)
+            {
+                await using var imageStream = request.Image.OpenReadStream();
+                
+                food.Image = !string.IsNullOrEmpty(food.Image) 
+                    ? await fileStorageService.UpdateImageAsync(imageStream, Guid.NewGuid() + ".jpg", food.Image)
+                    : await fileStorageService.UploadImageAsync(imageStream, Guid.NewGuid() + ".jpg");
+            }
 
+            mapper.Map(request, food);
+            
             await foodRepository.SaveChangesAsync(cancellationToken);
 
             return mapper.Map<FoodDto>(food);
